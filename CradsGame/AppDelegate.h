201b4c5b788bf2842ac85683effd78a217e8871d
Odelya Krief <okrief@lightricks.com>
2019-02-03 18:31:17 +0200

    Initial Commit

---
+++ CradsGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CradsGame
+//
+//  Created by Odelya Krief on 03/02/2019.
+//  Copyright © 2019 Odelya Krief. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CradsGame/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CradsGame
+//
+//  Created by Odelya Krief on 03/02/2019.
+//  Copyright © 2019 Odelya Krief. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
